3aec346e0728dbd8b936d03af8329d39d39b048f
drakeknapper <dknapper1@student.gsu.edu>
2016-01-27 15:30:53 -0500

    first

---
+++ plant.c
@@ -0,0 +1,126 @@
+
+/*
+ * Three-cell Leech heart interneuron model is implimented 
+ * as in the draft  "Bifurcations of bursting polyrhythms
+ * in 3-cell central pattern generators" of Jeremy Wojcik,
+ * Robert Clewley, and Andrey Shilnikov, prepared for
+ * Journal of Neuroscience 2012.
+ */
+
+
+#include <math.h>
+#include <stdio.h>
+
+
+#define N_EQ1	6
+#define N_EQ3	3*N_EQ1
+#define N_EQ4	4*N_EQ1
+
+#define C_m	1.	  // uF/cm^2
+#define V_Na	30.	  // mV
+//#define V_Ca	140.	  // mV
+#define V_K	-75.	  // mV
+#define V_L	-40.	  // mV
+#define g_Na	4.	// mmho/cm^2
+#define g_Ca	0.004	// mmho/cm^2
+#define g_K	0.3	// mmho/cm^2
+#define g_K_Ca	0.03	// mmho/cm^2
+#define g_L	0.003	// mmho/cm^2
+#define C_1	127./105.
+#define C_2	8265./105.
+#define lambda	1./12.5
+#define tau_x	235.	// ms ??
+#define A	0.15	// ??
+#define Bt	0.02
+#define th	20
+#define B	-50.	// ??
+#define rho	0.0003	// ms^-1
+#define K_c	0.0085	// mV^-1
+#define THRESHOLD_SLOPE	1.	// mV^-1
+#define THRESHOLD	-30.  // mV
+#define E_syn	-62.5  // mV
+//#define <++>	<++>	// <++>
+//#define <++>	<++>	// <++>
+//#define <++>	<++>	// <++>
+
+
+
+double alpha_m(const double V)		{ return 0.1*(50.-V)/(exp((50.-V)/10.)-1.); } 
+double beta_m(const double V)		{ return 4.*exp((25.-V)/18.); } 
+double alpha_h(const double V)		{ return 0.07*exp((25.-V)/20.); } 
+double beta_h(const double V)		{ return 1./(1.+exp((55.-V)/10.)); } 
+double alpha_n(const double V)		{ return 0.01*(55.-V)/(exp((55.-V)/10.)-1.); } 
+double beta_n(const double V)		{ return 0.125*exp((45.-V)/80.); }
+
+double m_inf(const double V_tilde)	{ return alpha_m(V_tilde)/(alpha_m(V_tilde)+beta_m(V_tilde)); } 
+double h_inf(const double V_tilde)	{ return alpha_h(V_tilde)/(alpha_h(V_tilde)+beta_h(V_tilde)); }
+double tau_h(const double V_tilde)	{ return 1./(alpha_h(V_tilde)+beta_h(V_tilde)); }
+double n_inf(const double V_tilde)	{ return alpha_n(V_tilde)/(alpha_n(V_tilde)+beta_n(V_tilde)); }
+double tau_n(const double V_tilde)	{ return 1./(alpha_n(V_tilde)+beta_n(V_tilde)); }
+double x_inf(const double V)		{ return 1./(1.+exp(A*(B-V))); }
+
+void derivs_one(const double* y, double* dydt, const double* p)
+{
+	double V=y[0], h=y[1], n=y[2], x=y[3], Ca=y[4], S=y[5];
+	double V_tilde = C_1*V+C_2, V_Ca=p[0];
+
+	dydt[0] = -g_Na*powf(m_inf(V_tilde), 3)*h*(V-V_Na) - g_Ca*x*(V-V_Ca) - (g_K*powf(n, 4)+g_K_Ca*Ca/(0.5+Ca))*(V-V_K) - g_L*(V-V_L);	// dV/dt
+        dydt[1] = lambda*(h_inf(V_tilde)-h)/tau_h(V_tilde);	// dh/dt
+        dydt[2] = lambda*(n_inf(V_tilde)-n)/tau_n(V_tilde);	// dn/dt
+        dydt[3] = (x_inf(V)-x)/tau_x;				// dx/dt
+        dydt[4] = rho * (K_c * x * (V_Ca - V) - Ca);		// d[Ca2+]/dt
+	dydt[5] = A*(1.-S)/(1.+exp(-k*(V-th))) - Bt*S         // dS/dt
+}
+
+
+
+double boltzmann(const double x, const double x_0, const double k)
+{
+	return 1./(1.+exp(-k*(x-x_0)));
+}
+
+
+
+void integrate_one_rk4(double* y, const double dt, const unsigned N, const unsigned stride, const double* P, double* output)
+{
+	unsigned i, j, k;
+	double dt2, dt6;
+	double y1[N_EQ1], y2[N_EQ1], k1[N_EQ1], k2[N_EQ1], k3[N_EQ1], k4[N_EQ1];
+	dt2 = dt/2.; dt6 = dt/6.;
+
+	for(j=0; j<N_EQ1; j++)
+		output[j] = y[j];
+
+
+
+	for(i=1; i<N; i++)
+	{
+		for(j=0; j<stride; j++)
+		{
+			derivs_one(y, k1, P);
+			for(k=0; k<N_EQ1; k++)
+			       y1[k] = y[k]+k1[k]*dt2; 			
+
+			derivs_one(y1, k2, P);
+			for(k=0; k<N_EQ1; k++)
+				y2[k] = y[k]+k2[k]*dt2; 			
+
+			derivs_one(y2, k3, P);
+			for(k=0; k<N_EQ1; k++)
+				y2[k] = y[k]+k3[k]*dt; 			
+
+			derivs_one(y2, k4, P);
+			for(k=0; k<N_EQ1; k++)
+				y[k] += dt6*(k1[k]+2.*(k2[k]+k3[k])+k4[k]);
+		}
+		//printf("%i %lf\n", i, y[0]);
+		for(j=0; j<N_EQ1; j++) output[N_EQ1*i+j] = y[j];
+	}
+}
+
+
+
+
+
+
+
---
+++ plant.py
@@ -0,0 +1,264 @@
+#! /usr/bin/python
+
+import ctypes as ct
+import time
+import sys
+import os
+import Motiftoolbox.Tools.tools as tl
+import numpy as np
+import scipy.optimize as opt
+import scipy.interpolate as trp
+
+lib = ct.cdll.LoadLibrary(os.path.dirname(__file__)+'/lib/_plant.so')
+
+
+CUDA_ENABLED = False
+try:
+	lib_cuda = ct.cdll.LoadLibrary(os.path.dirname(__file__)+'/lib/_plant_cuda.so')
+	CUDA_ENABLED = True
+except:
+	pass
+
+#===
+
+PI2 = 2.*np.pi
+
+params = {}
+params['A'] = 0.15	  # ??
+params['B'] = -50.	  # ??
+params['C_1'] = 127./105.	  # ??
+params['C_2'] = 8265./105.	  # ??
+params['K_c'] = 0.0085	  # mV^-1
+params['V_Ca'] = 140.	  # mV
+#params['<++>'] = <++>	  # <++>
+#params['<++>'] = <++>	  # <++>
+#params['<++>'] = <++>	  # <++>
+
+N_EQ1 = 6
+N_EQ3 = 3*N_EQ1
+N_EQ4 = 4*N_EQ1
+
+
+def params_one():
+	return np.array([params['V_Ca']])
+
+
+def params_three():
+	return np.array([params['V_Ca']])
+
+
+"""#===
+
+if CUDA_ENABLED:
+	
+	lib_cuda.cuda_integrate_three.argtype = [ct.POINTER(ct.c_double), ct.c_uint,
+				    	ct.POINTER(ct.c_double), ct.c_uint,
+				    	ct.POINTER(ct.c_double), ct.c_uint,
+				    	ct.POINTER(ct.c_double), ct.c_uint,
+				    	ct.c_double, ct.c_uint, ct.c_uint]
+	def cuda_integrate_three_rk4(initial_states, coupling, dt, N_integrate, stride=1):
+		coup = np.zeros((9), float)
+		coup[:coupling.size] = coupling
+	
+		initial_states = np.asarray(initial_states)
+		N_initials = initial_states.size/9 # initial states / state variables
+		X_out = np.zeros((3*N_initials*N_integrate), float)
+		p = params_three()
+	
+		lib_cuda.cuda_integrate_three(initial_states.ctypes.data_as(ct.POINTER(ct.c_double)), ct.c_uint(initial_states.size),
+					X_out.ctypes.data_as(ct.POINTER(ct.c_double)),
+					coup.ctypes.data_as(ct.POINTER(ct.c_double)),
+					p.ctypes.data_as(ct.POINTER(ct.c_double)),
+					ct.c_double(dt), ct.c_uint(N_integrate), ct.c_uint(stride))
+	
+		return np.reshape(X_out, (N_initials, N_integrate, 3), 'C')
+	
+	
+#===
+"""
+
+lib.derivs_one.argtypes = [ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double)]
+def derivs_one(state):
+	state = np.array(state, dtype=float)
+	derivative = np.zeros((3), float)
+	parameters = params_one()
+	lib.derivs_one(state.ctypes.data_as(ct.POINTER(ct.c_double)),
+			derivative.ctypes.data_as(ct.POINTER(ct.c_double)),
+			parameters.ctypes.data_as(ct.POINTER(ct.c_double)))
+
+	return derivative
+
+
+
+lib.integrate_one_rk4.argtypes = [ct.POINTER(ct.c_double), ct.c_double, ct.c_uint, ct.c_uint, ct.POINTER(ct.c_double), ct.POINTER(ct.c_double)]
+def integrate_one_rk4(initial_state, dt, N_integrate, stride=1):
+	initial_state = np.asarray(initial_state)
+	assert initial_state.size == N_EQ1
+	X_out = np.zeros((N_EQ1*N_integrate), float)
+	parameters = params_one()
+	lib.integrate_one_rk4(initial_state.ctypes.data_as(ct.POINTER(ct.c_double)),
+		ct.c_double(dt), ct.c_uint(N_integrate), ct.c_uint(stride),
+		parameters.ctypes.data_as(ct.POINTER(ct.c_double)),
+		X_out.ctypes.data_as(ct.POINTER(ct.c_double)))
+
+	return np.reshape(X_out, (N_EQ1, N_integrate), 'F')
+
+#===
+
+initial_state = [-42.13934349, 0.51379418, 0.10720682, 0.74274042, 0.77535553]
+dt = 1.0
+N_integrate = 5*10**4
+stride = 10
+THRESHOLD = -30.6
+IDX_THRESHOLD = 0
+
+def single_orbit(dt=dt, N_integrate=N_integrate, stride=stride, V_threshold=THRESHOLD):
+	X = integrate_one_rk4(initial_state, dt=dt/float(stride), N_integrate=N_integrate, stride=stride)
+	V, h, n, x, Ca, S= X[0], X[1], X[2], X[3], X[4], X[5]
+	V_model, h_model, n_model, x_model, Ca_model, S_model =  tl.splineLS1D(), tl.splineLS1D(), tl.splineLS1D(), tl.splineLS1D(), tl.splineLS1D(), tl.splineLS1D()
+
+	try:
+		ni = tl.crossings(V, V_threshold) # convert to millivolts
+		V, h, n, x, Ca, S = V[ni[-2]:ni[-1]], h[ni[-2]:ni[-1]], n[ni[-2]:ni[-1]], x[ni[-2]:ni[-1]], Ca[ni[-2]:ni[-1]], S[ni[-2]:ni[-1]]
+		t = PI2*np.arange(V.size)/float(V.size-1)
+		V_model.makeModel(V, t); h_model.makeModel(h, t); n_model.makeModel(n, t); x_model.makeModel(x, t); Ca_model.makeModel(Ca, t); S_model.makeModel(S, t)
+
+	except:
+		print '# single_orbit:  No closed orbit found!'
+		raise ValueError
+
+	
+	T = dt*V.size
+
+	return V_model, h_model, n_model, x_model, Ca_model, S_model, T
+
+
+#===
+
+"""
+lib.integrate_three_rk4.argtypes = [ct.POINTER(ct.c_double),
+					ct.POINTER(ct.c_double),
+					ct.POINTER(ct.c_double),
+					ct.POINTER(ct.c_double),
+					ct.c_double, ct.c_uint, ct.c_uint]
+def integrate_three_rk4(initial_state, coupling, dt, N_integrate, stride=1):
+	coup = np.zeros((9), float)
+	coup[:coupling.size] = coupling
+
+	initial_state = np.asarray(initial_state)
+	X_out = np.zeros((3*N_integrate), float)
+	parameters = params_three()
+
+	lib.integrate_three_rk4(initial_state.ctypes.data_as(ct.POINTER(ct.c_double)),
+				parameters.ctypes.data_as(ct.POINTER(ct.c_double)),
+				coupling.ctypes.data_as(ct.POINTER(ct.c_double)),
+				X_out.ctypes.data_as(ct.POINTER(ct.c_double)),
+				ct.c_double(dt), ct.c_uint(N_integrate), ct.c_uint(stride))
+
+	return np.reshape(X_out, (N_integrate, 3), 'C')
+
+
+
+lib.integrate_four_rk4.argtypes = [ct.POINTER(ct.c_double),
+					ct.POINTER(ct.c_double),
+					ct.POINTER(ct.c_double),
+					ct.POINTER(ct.c_double),
+					ct.c_double, ct.c_uint, ct.c_uint]
+def integrate_four_rk4(initial_states, coupling, dt, N_integrate, stride=1):
+	initial_states = np.asarray(initial_states) #
+	assert initial_states.size == N_EQ4
+
+	coup = np.zeros((18), float)
+	coup[:coupling.size] = np.asarray(coupling);
+
+	X_out = np.zeros((4*N_integrate), float)
+	p = params_three()
+
+	lib.integrate_four_rk4(initial_states.ctypes.data_as(ct.POINTER(ct.c_double)),
+		p.ctypes.data_as(ct.POINTER(ct.c_double)),
+		coup.ctypes.data_as(ct.POINTER(ct.c_double)),
+		X_out.ctypes.data_as(ct.POINTER(ct.c_double)),
+		ct.c_double(dt), ct.c_uint(N_integrate), ct.c_uint(stride))
+
+	return np.reshape(X_out, (N_integrate, 4), 'C')
+"""
+
+def alpha_m(V): return 0.1*(50.-V)/(np.exp((50.-V)/10.)-1.)
+def beta_m(V):  return 4.*np.exp((25.-V)/18.)
+def alpha_h(V): return 0.07*np.exp((25.-V)/20.)
+def beta_h(V):  return 1./(1.+np.exp((55.-V)/10.))
+def alpha_n(V): return 0.01*(55.-V)/(np.exp((55.-V)/10.)-1.)
+def beta_n(V):  return 0.125*np.exp((45.-V)/80.)
+
+def m_inf(V_tilde):	return alpha_m(V_tilde)/(alpha_m(V_tilde)+beta_m(V_tilde)) 
+def h_inf(V_tilde):	return alpha_h(V_tilde)/(alpha_h(V_tilde)+beta_h(V_tilde))
+def tau_h(V_tilde):	return 1./(alpha_h(V_tilde)+beta_h(V_tilde))
+def n_inf(V_tilde):	return alpha_n(V_tilde)/(alpha_n(V_tilde)+beta_n(V_tilde))
+def tau_n(V_tilde):	return 1./(alpha_n(V_tilde)+beta_n(V_tilde))
+def x_inf(V):		return 1./(1.+np.exp(params['A']*(params['B']-V)))
+def Vx_inf(x):	return params['B']-np.log(1./x-1.)/params['A']
+
+def nullcline_h(V): # nullcline_h
+	return h_inf(params['C_1']*V+params['C_2'])
+
+
+def nullcline_n(V): # nullcline_n
+	return n_inf(params['C_1']*V+params['C_2'])
+
+
+def nullcline_x(x): # nullcline_x
+
+	def func(Ca, x):
+		return x_inf(params['V_Ca']-Ca/params['K_c']/x)-x
+
+	Ca = list(opt.fsolve(func, [0.17], args=(x[0],)))
+	for i in xrange(1, x.size):
+		Ca.append(opt.fsolve(func, [Ca[-1]], args=(x[i],))[0])
+
+	return Ca
+
+
+def nullcline_Ca(x): # intersection of nullcline x and nullcline Ca
+	return params['K_c']*x*(params['V_Ca']-Vx_inf(x))
+
+
+
+if __name__ == '__main__':
+
+	from pylab import *
+	import time
+
+	dt = 1.0
+	stride = 10
+	N = 5*10**4
+	t = dt*arange(N)
+	
+	"""
+	X_in  =  concatenate((initial_state, initial_state, zeros((10), float)))
+	V = integrate_four_rk4(X_in, coupling=zeros((18), float), dt=dt/float(stride), N_integrate=N, stride=stride)
+	plot(V[:, 0])
+	plot(V[:, 1])
+	plot(V[:, 2])
+	plot(V[:, 3])
+	show()
+
+	"""
+	X = integrate_one_rk4(initial_state, dt=dt/float(stride), N_integrate=N, stride=stride)
+
+	ax = subplot(511)
+	plot(t, X[0], 'k.-')
+	ylabel('V')
+	subplot(512, sharex=ax)
+	plot(t, X[1], 'k.-')
+	ylabel('h')
+	subplot(513, sharex=ax)
+	plot(t, X[2], 'k.-')
+	ylabel('n')
+	subplot(514, sharex=ax)
+	plot(t, X[3], 'k.-')
+	ylabel('x')
+	subplot(515, sharex=ax)
+	plot(t, X[4], 'k.-')
+	ylabel('[Ca2+]')
+	show()
+
